e73a1068c8427d3688c08c8df9cafcb2cda0f527
Luiz Felipe <luizfmsilva29082005@gmail.com>
2026-03-13 17:14:54 -0300

    Adicionar Controller Usuario

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -1,12 +1,52 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using Microsoft.EntityFrameworkCore;
+using projetoIntegradorOlhuz.API.Data;
+using projetoIntegradorOlhuz.API.Models;
+using projetoIntegradorOlhuz.API.Models.DTO;
+
 
 namespace projetoIntegradorOlhuz.API.Controllers
 {
+    //para habilitar a api
+    [ApiController]
+
+    [Route("api/[controller]")]
     public class UsuarioController : Controller
     {
-        public IActionResult Index()
+        [HttpPost("CriarUsuario")]
+        public async Task<IActionResult> CriarUsuario([FromBody] CriarUsuario usuario)
         {
-            return View();
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            Usuario? usuarioEncontrado = await _usuarioDbContext,Usuario.FirsOrDeFaultAsync(usuario => Usuario.CPF == dadosUsuario.CPF);
+          
+            if (UsuarioEncontrado != null)
+            {
+                return BadRequest($"Já existe um usuario cadastrado com o CPF {dadosUsuario.CPF}");
+            }
+
+            Usuario usuario = new usuario
+            {
+                Nome = dadosUsuario.Nome,
+                CPF = dadosUsuario.CPF,
+                DataNascimento = dadosUsuario.DataNascimento,
+                Telefone = dadosUsuario.Telefone,
+                Email = dadosUsuario.Email,
+                Senha = dadosUsuario.Senha
+            };
+
+            _usuarioDbContext.Usuario.Add(usuario);
+            int resultadoGravacao = await _usuarioDbContext.SaveChangesAsync();
+
+            if (resultadoGravacao > 0)
+            {
+                return Created();
+            }
+
+            return BadRequest("Erro ao criar usuario");
         }
     }
 }
