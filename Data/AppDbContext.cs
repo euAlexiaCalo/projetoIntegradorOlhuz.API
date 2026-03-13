b030bac389c85f0b3dcffc15914eca40a626d57b
Raí <rai.csilva@senacsp.edu.br>
2026-03-13 15:42:43 -0300

    Alteração na AppDbContext.

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,6 +1,19 @@
-﻿namespace projetoIntegradorOlhuz.API.Data
+﻿using System.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using projetoIntegradorOlhuz.API.Models;
+
+namespace projetoIntegradorOlhuz.API.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext 
     {
+        public  AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+         
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+       
+
     }
 }
