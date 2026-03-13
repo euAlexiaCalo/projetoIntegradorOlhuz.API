312eec3cfb0efa2ecaee8cd2b8da41e331b86a62
Aléxia <alexiacalovj@outlook.com>
2026-03-13 14:35:38 -0300

    Ignorando arquivo e editando Program

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
 
+using Microsoft.EntityFrameworkCore;
+using projetoIntegradorOlhuz.API.Data;
+using Scalar.AspNetCore;
+
 namespace projetoIntegradorOlhuz.API
 {
     public class Program
@@ -8,6 +12,9 @@ namespace projetoIntegradorOlhuz.API
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+
+            // Adicionado para ligar com o banco de dados
+            //**** builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             builder.Services.AddControllers();
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -19,6 +26,7 @@ namespace projetoIntegradorOlhuz.API
             if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
+                app.MapScalarApiReference(); // Inicializar o Scalar
             }
 
             app.UseHttpsRedirection();
