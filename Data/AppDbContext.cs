d0f1aeb3cdaa444cb4a58983daee6a23c42c1983
Aléxia <alexiacalovj@outlook.com>
2026-03-13 16:30:49 -0300

    Acrescentar Enum

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,6 +1,18 @@
-﻿namespace projetoIntegradorOlhuz.API.Data
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using OlhuzApiWeb.Model;
+using projetoIntegradorOlhuz.API.Models;
+using System.Data;
+
+namespace projetoIntegradorOlhuz.API.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Configuracao> Configuracoes { get; set; }
     }
 }
